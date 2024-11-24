34941564a42c1186be053abe4033381ff464b26f
Mario Wibisono <kobamkode@gmail.com>
2024-11-24 07:26:24 +0700

    feat: add func

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,4 @@
-import type { PageServerLoad } from "./$types";
+import type { PageServerLoad, Actions } from "./$types";
 import PocketBase from "pocketbase";
 
 const pb = new PocketBase("http://127.0.0.1:8090");
@@ -7,6 +7,17 @@ export const load: PageServerLoad = async () => {
 	const tasks = await pb.collection('tasks').getFullList({
 		sort: '-created',
 	});
-
 	return { tasks };
 }
+
+export const actions = {
+	default: async ({ request }) => {
+		const data = await request.formData();
+		const taskName = data.get('name');
+		const task = {
+			"name": taskName
+		}
+		const add = await pb.collection('tasks').create(task);
+		console.log(add);
+	}
+} satisfies Actions;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,19 +2,23 @@
 	let { data } = $props();
 </script>
 
-<h1 class="text-xl">Todo - SvelteKit</h1>
+<h1 class="text-xl">Todo - SvelteKit + PocketBase</h1>
 
 <div class="mt-5">
 	<form action="" method="post">
-		<input type="text" placeholder="What do you want to do?" />
+		<input type="text" name="name" placeholder="What do you want to do?" />
 		<button type="submit" class="py-2 px-4 border">Add</button>
 	</form>
 </div>
 
 <div class="mt-5">
 	<ul>
-		{#each data.tasks as task}
-			<li>{task.name}</li>
-		{/each}
+		{#if data.tasks.length > 0}
+			{#each data.tasks as task}
+				<li>{task.name}</li>
+			{/each}
+		{:else}
+			<li>No tasks!</li>
+		{/if}
 	</ul>
 </div>
